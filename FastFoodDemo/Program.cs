c34eab8d172becfa8ea26465c9bd256352efdc51
BayuGoldenZ <bayu@telkomschools.sch.id>
2022-08-24 18:09:22 +0700

    Update

--- FastFoodDemo/Program.cs
+++ FastFoodDemo/Program.cs
@@ -16,7 +16,7 @@ namespace FastFoodDemo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMenuUtama());
+            Application.Run(new login());
         }
     }
 }
